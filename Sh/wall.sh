59a23ceb34e15ae4ae6a4cf3f9cb4b4e074375bf
sudev <sudev@sudev.(none)>
2012-11-08 19:26:04 +0530

    wallpaper sh comments

--- Sh/wall.sh
+++ Sh/wall.sh
@@ -2,15 +2,10 @@
 
 #Inspired from jaseem abid to set a wallpaper in Xfce from Natgeo
 # Make cache directory
-dir=~/.wpoftheday
-[ -d $dir ] || mkdir $dir
+#dir=~/.wpoftheday
 
-# CD to cache dir
-cd $dir
-# Delete index file if it exists
-[ -e p1 ] || rm p1
-[ -e p2 ] || rm p2
 
+echo -ne "Fecthing link.. \n"
 page="http://photography.nationalgeographic.com/photography/photo-of-the-day/"
 wget -q -O p1  $page
 
@@ -24,8 +19,20 @@ wget -q -O p2  $page$prev
 link=$(cat p2 | grep "Download Wallpaper" |  grep -Po 'http.*.jpg')
 
 #wget -c -P /home/sudev/Downloads/Natgeo --progress=bar:force $link
-echo -n "Downloading $prev:.."
-echo -n "    "
-wget --progress=dot $link 2>&1 | grep --line-buffered "%" | sed -u -e "s,\.,,g" | awk '{printf("\b\b\b\b%4s", $2)}'
+echo -ne "Downloading $prev:..\n"
+wget -nvc $link
 echo -ne "\b\b\b\b"
-echo " DONE"
+echo -ne "Done Downloading \n"
+file=$(echo  $link |  grep -Po '[-\w]*.jpg')
+# Set picture $file as desktop background
+# 0 - Auto, 1 - Centered, 2 - Tiled, 3 - Stretched, 4 - Scaled, 5 - Zoomed
+mv $file 1.jpg
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-path -s 1.jpg
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-style -s 0
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-show -s false
+xfconf-query -c xfce4-desktop -p /backdrop/screen0/monitor0/image-show -s true
+echo -ne "\nWallpaper set \n Saved to ~/Natgeo\n"
+[ -d ~/Natgeo ] || mkdir ~/Natgeo
+mv 1.jpg ~/Natgeo/`date +%d-%m-%Y`.jpg
+# Delete index file if it exists
+rm p1 p2
