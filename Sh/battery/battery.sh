9163b04bdbc9b10443b08c73977db54b5ec5a818
sudev <sudevdev@gmail.com>
2014-02-27 15:51:45 +0530

    added immediate file flag

--- Sh/battery/battery.sh
+++ Sh/battery/battery.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 #Author Sudev Ambadi
+#crontab to be created to run the script so that we can track the battery performance 
+#crontab entry - */5 * * * * /path-to-this script
     date >> /home/sudev/.batlog
     upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep -E "state|time|per" >> /home/sudev/.batlog
     echo $'\n' >> /home/sudev/.batlog
